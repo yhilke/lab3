2a6bb4f4a2383501a847648f3517bac5682204af
Yoshika Hilke <yhilke.ucsd.edu>
2017-10-30 00:43:16 -0700

    styling

--- js/introHCI.js
+++ js/introHCI.js
@@ -12,7 +12,18 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
+	$("a.thumbnail").click(projectClick);
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	}
+function projectClick(e) {
+	console.log("Project Clicked");
+	e.preventDefault();
+
+	var. containingProject = $(this).closest("project");
+
+	containingProject.append("<div class='project-description'><p><Description of the project.</p></div>")
+	//prevents page from reloading
+
+	$(this).css("background-color", "#7fff00");
+}
 }
